c52d7f828f3298fc86a8129a5096ca8bdd4218b4
sujin <atena9604@naver.com>
2024-01-28 22:39:47 +0900

    [feat] styled component 추가

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Title = styled.h1`
+color: pink`;
+export default function Nav() {
+    return
+    <div>
+        <Title>test</Title>
+        Navigation
+    </div>
+}
